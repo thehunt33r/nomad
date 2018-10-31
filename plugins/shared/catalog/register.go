30d3025e547a6fa00926603d527c708094b87ad0
Mahmood Ali <mahmood@hashicorp.com>
2018-10-31 12:25:46 -0400

    register the java plugin

--- plugins/shared/catalog/register.go
+++ plugins/shared/catalog/register.go
@@ -1,6 +1,7 @@
 package catalog
 
 import (
+	"github.com/hashicorp/nomad/drivers/java"
 	"github.com/hashicorp/nomad/drivers/qemu"
 	"github.com/hashicorp/nomad/drivers/rawexec"
 )
@@ -11,4 +12,5 @@ import (
 func init() {
 	RegisterDeferredConfig(rawexec.PluginID, rawexec.PluginConfig, rawexec.PluginLoader)
 	Register(qemu.PluginID, qemu.PluginConfig)
+	Register(java.PluginID, java.PluginConfig)
 }
